0a6b70b1a21d86fbb269f9184543bd046c132829
Tobias Schmid <toashd@gmail.com>
2023-06-26 11:17:03 +0200

    Use netip.Addr instead of string

--- pkg/api/filter.go
+++ pkg/api/filter.go
@@ -2,8 +2,8 @@ package api
 
 import (
 	"fmt"
-	"net"
 	"net/http"
+	"net/netip"
 	"strings"
 )
 
@@ -16,7 +16,7 @@ var defaultBlockedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(http.StatusUnauthorized)
-	fmt.Fprintln(w, ErrMsgUnauthorized)
+	_, _ = fmt.Fprintln(w, ErrMsgUnauthorized)
 })
 
 // IPFilter implements a simple IP filter. It is configured with an access control list
@@ -25,20 +25,20 @@ var defaultBlockedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http
 type IPFilter struct {
 	// allowedIPs is the list of IPs allowed to access a resource.
 	// If empty, the IP filter is disabled and every request is allowed.
-	allowedIPs map[string]struct{}
+	allowedIPs map[netip.Addr]struct{}
 }
 
 // NewIPFilter create a new IP filter.
 func NewIPFilter(whitelist string) *IPFilter {
 	f := &IPFilter{
-		allowedIPs: make(map[string]struct{}),
+		allowedIPs: make(map[netip.Addr]struct{}),
 	}
 
 	// Parse allowed IPs from config.
 	if ips := strings.Trim(whitelist, ","); len(ips) > 0 {
 		for _, ip := range strings.Split(ips, ",") {
-			if ipp := net.ParseIP(strings.TrimSpace(ip)); ipp != nil {
-				f.allowedIPs[ipp.String()] = struct{}{}
+			if addr, err := netip.ParseAddr(strings.TrimSpace(ip)); err == nil {
+				f.allowedIPs[addr] = struct{}{}
 			}
 		}
 	}
@@ -54,10 +54,15 @@ func (f *IPFilter) Wrap(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if len(f.allowedIPs) == 0 {
 			next(w, r)
+			return
 		}
 
 		// Get the original client IP.
-		ip := originalIP(r)
+		ip, err := originalIP(r)
+		if err != nil {
+			defaultBlockedHandler.ServeHTTP(w, r)
+			return
+		}
 
 		// Check if the IP is allowed or blocked.
 		if !f.IsAllowed(ip) {
@@ -70,7 +75,7 @@ func (f *IPFilter) Wrap(next http.HandlerFunc) http.HandlerFunc {
 }
 
 // IsAllowed checks if the given IP is allowed.
-func (f *IPFilter) IsAllowed(ip string) bool {
+func (f *IPFilter) IsAllowed(ip netip.Addr) bool {
 	if _, ok := f.allowedIPs[ip]; ok {
 		return true
 	}
@@ -78,26 +83,25 @@ func (f *IPFilter) IsAllowed(ip string) bool {
 }
 
 // originalIP finds the originating client IP.
-func originalIP(req *http.Request) string {
-	addr := ""
+func originalIP(req *http.Request) (netip.Addr, error) {
 	// The default is the originating IP. But, we try to find better
 	// options as this is almost never the right IP we're looking for.
+	addr := ""
 	if parts := strings.Split(req.RemoteAddr, ":"); len(parts) == 2 {
 		addr = parts[0]
 	}
+
 	// If we have a forwarded-for header, take the address from there.
 	if xff := strings.Trim(req.Header.Get("X-Forwarded-For"), ","); len(xff) > 0 {
 		addrs := strings.Split(xff, ",")
 		last := addrs[len(addrs)-1]
-		if ip := net.ParseIP(last); ip != nil {
-			return ip.String()
-		}
+		return netip.ParseAddr(last)
 	}
+
 	// Otherwise, parse the X-Real-Ip header if it exists.
 	if xri := req.Header.Get("X-Real-Ip"); len(xri) > 0 {
-		if ip := net.ParseIP(xri); ip != nil {
-			return ip.String()
-		}
+		return netip.ParseAddr(xri)
 	}
-	return addr
+
+	return netip.ParseAddr(addr)
 }
